af8fc120fe5889600f3a34f3c4ac49baf67d13f2
Diomedes Git <diomedesgit@gmail.com>
2026-01-16 02:33:58 +0000

    test: use full context for controller tests

--- divination_api/src/test/java/com/divinationengine/divination/controller/CardControllerTest.java
+++ divination_api/src/test/java/com/divinationengine/divination/controller/CardControllerTest.java
@@ -5,8 +5,9 @@ import com.divinationengine.divination.models.CardType;
 import com.divinationengine.divination.service.CardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -18,7 +19,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(CardController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @SuppressWarnings("null")
 class CardControllerTest {
 
--- divination_api/src/test/java/com/divinationengine/divination/controller/ReadingControllerTest.java
+++ divination_api/src/test/java/com/divinationengine/divination/controller/ReadingControllerTest.java
@@ -8,8 +8,9 @@ import com.divinationengine.divination.service.ReadingService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +24,8 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(ReadingController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @SuppressWarnings("null")
 class ReadingControllerTest {
 
