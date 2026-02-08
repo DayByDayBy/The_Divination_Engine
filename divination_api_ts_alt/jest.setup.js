0e5c0b5d14c95bfe94051a682ae334c55ba8865b
Diomedes Git <diomedesgit@gmail.com>
2026-02-08 23:53:51 +0000

    jwt mock add

--- divination_api_ts_alt/jest.setup.js
+++ divination_api_ts_alt/jest.setup.js
@@ -1,6 +1,9 @@
 // Jest setup file
 import 'jest-extended';
 
+// Set up test environment variables
+process.env.JWT_SECRET = 'test-secret-key-that-is-at-least-32-chars-long';
+
 // Mock console methods in tests to reduce noise
 global.console = {
   ...console,
