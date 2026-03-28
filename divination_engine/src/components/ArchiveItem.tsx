5ac811eff11735e2c11536b42e2df76f0dbb4242
Diomedes Git <diomedesgit@gmail.com>
2026-03-28 13:12:16 +0000

    ArchiveItem llm  version

--- divination_engine/src/components/ArchiveItem.tsx
+++ divination_engine/src/components/ArchiveItem.tsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Spread from "./Spread";
-import { CardItem, Reading } from "../types/index";
+import Reading from "./Reading";
+import { Reading as ReadingType } from "../types/index";
 
 interface ArchiveItemProps {
-    reading: Reading;
+    reading: ReadingType;
 }
 
-const ArchiveItem: React.FC<ArchiveItemProps> = ({reading}) => {
-
+const ArchiveItem: React.FC<ArchiveItemProps> = ({ reading }) => {
     return (
-    
-    
-    <>
-       <div className="reading-container">
-        <Spread cards={reading.cardReadings || []} />
+        <div className="reading-container">
+            <Spread cards={reading.cardReadings || []} />
+            <Reading cards={reading.cardReadings || []} />
+            {reading.llmInterpretation && (
+                <div className="interpretation">
+                    <h3>Interpretation</h3>
+                    <p>{reading.llmInterpretation}</p>
+                </div>
+            )}
         </div>
-    </>
-    )
+    );
 };
 
 export default ArchiveItem;
